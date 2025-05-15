92ba592b7f7bb8eaeb19c7540a97a618becaf751
Thomas Woltjer <thomas.woltjer@vibrationresearch.com>
2025-05-15 12:01:30 -0400

    Add comments around AlreadyReturnedBufferReturnBehavior values

--- BetterBuffers/AlreadyReturnedBufferReturnBehavior.cs
+++ BetterBuffers/AlreadyReturnedBufferReturnBehavior.cs
@@ -5,7 +5,16 @@ namespace BetterBuffers;
 /// </summary>
 public enum AlreadyReturnedBufferReturnBehavior
 {
+	/// <summary>
+	/// Ignore/don't check if a buffer has already been previously returned. Best performance, but can lead to unexpected problems if an issue exists in user code that violates expected behavior.
+	/// </summary>
 	Ignore,
+	/// <summary>
+	/// Checks if a buffer has been previously returned and is waiting to be rented, and triggers a Debug.Fail() if it has been.
+	/// </summary>
 	DebugFail,
+	/// <summary>
+	/// Checks if a buffer has been previously returned and is waiting to be rented, and throws an exception if it has been.
+	/// </summary>
 	ThrowException,
 }
